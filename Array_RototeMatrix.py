450a798ad117d5bb457ffc2452d44c3b2982794f
CHAWALE ADITYA SUDHAKAR <adityachawale14@gmail.com>
2024-07-23 12:55:45 +0530

    Day-8

--- Array_RototeMatrix.py
+++ Array_RototeMatrix.py
@@ -1,6 +1,6 @@
 import numpy as np
 arr=np.array([[1,2,3],[4,5,6],[7,8,9]])
-print(len(arr))
+# print(len(arr))
 def rotateMatrix(arr):
     n=len(arr)
     for i in range(n):                                  
