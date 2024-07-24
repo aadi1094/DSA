333d954a667b96f561e32e5be070dd570642266b
CHAWALE ADITYA SUDHAKAR <adityachawale14@gmail.com>
2024-07-24 11:43:25 +0530

    Day-9

---
+++ SLL_Search_Deletion.py
@@ -0,0 +1,121 @@
+# Define the Node class
+class Node:
+    def __init__(self,value=None):
+        self.value = value
+        self.next = None
+    
+# Define the Singly Linked List class
+class SLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    # Define the iterator for the linked list
+    def __iter__(self):
+        node = self.head
+        while node:
+            yield node
+            node = node.next
+
+    # Insert a node in the linked list
+    def insertSLL(self,value,location):
+        newNode = Node(value)  # Create a new node with the given value
+        if self.head is None:  # Check if the linked list is empty
+            self.head = newNode  # Set the new node as the head and tail
+            self.tail = newNode
+        else:
+            if location == 0:  # Check if the new node should be inserted at the beginning
+                newNode.next = self.head  # Set the next pointer of the new node to the current head
+                self.head = newNode  # Set the new node as the new head
+            elif location == 1:  # Check if the new node should be inserted at the end
+                self.tail.next = newNode  # Set the next pointer of the current tail to the new node
+                self.tail = newNode  # Set the new node as the new tail
+            else:  # If the new node should be inserted at a specific location
+                tempNode = self.head  # Start from the head of the linked list
+                index = 0  # Initialize the index counter
+                while index < location - 1 and tempNode is not None:  # Traverse the linked list until the desired location is reached or the end is reached
+                    tempNode = tempNode.next  # Move to the next node
+                    index += 1  # Increment the index counter
+                nextNode = tempNode.next  # Store the next node after the desired location
+                tempNode.next = newNode  # Set the next pointer of the current node to the new node
+                newNode.next = nextNode  # Set the next pointer of the new node to the next node after the desired location
+                if tempNode == self.tail:  # Check if the new node is inserted at the end
+                    self.tail = newNode  # Update the tail to the new node if necessary
+
+# Traverse the linked list
+    def traverseSLL(self):
+        if self.head is None:  # Check if the linked list is empty
+            print("The Singly Linked List does not exist")
+        else:
+            node = self.head  # Start from the head of the linked list
+            while node is not None:  # Traverse the linked list until the end is reached
+                print(node.value)  # Print the value of the current node
+                node = node.next  # Move to the next node
+
+    # Search for a node in the linked list
+    def searchSLL(self,nodeValue):
+        if self.head is None:  # Check if the linked list is empty
+            return "The Singly Linked List does not exist"
+        else:
+            node = self.head  # Start from the head of the linked list
+            while node is not None:  # Traverse the linked list until the end is reached
+                if node.value == nodeValue:  # Check if the value of the current node matches the search value
+                    return node.value  # Return the search value if found
+                node = node.next  # Move to the next node
+            return "The node does not exist in this Singly Linked List"
+        
+    # Delete a node from the linked list
+    def deleteNode(self,location):
+        if self.head is None:  # Check if the linked list is empty
+            print("The Singly Linked List does not exist")# Print a message if the linked list does not exist
+        else:
+            if location == 0:# Check if the node to be deleted is the head
+                if self.head == self.tail:# Check if the linked list has only one node
+                    self.head = None # Set the head to None
+                    self.tail = None # Set the tail to None
+                else:
+                    self.head = self.head.next # Set the next node as the new head
+            elif location == 1: # Check if the node to be deleted is the tail
+                if self.head == self.tail:# Check if the linked list has only one node
+                    self.head = None
+                    self.tail = None
+                else:
+                    node = self.head # Start from the head of the linked list
+                    while node is not None: # Traverse the linked list until the end is reached
+                        if node.next == self.tail: # Check if the next node is the tail
+                            break
+                        node = node.next # Move to the next node
+                    node.next = None # Set the next pointer of the current node to None
+                    self.tail = node # Set the current node as the new tail
+            else:
+                tempNode = self.head # Start from the head of the linked list
+                index = 0 # Initialize the index counter
+                while index < location - 1 and tempNode is not None: # Traverse the linked list until the desired location is reached or the end is reached
+                    tempNode = tempNode.next # Move to the next node
+                    index += 1
+                nextNode = tempNode.next # Store the next node after the desired location
+                tempNode.next = nextNode.next    # Set the next pointer of the current node to the node after the next node   
+
+# Create an instance of the Singly Linked List
+singlyLinkedList = SLinkedList()
+
+# Insert nodes into the linked list
+singlyLinkedList.insertSLL(1,1)
+singlyLinkedList.insertSLL(2,1)
+singlyLinkedList.insertSLL(3,1)
+singlyLinkedList.insertSLL(4,1)
+
+singlyLinkedList.insertSLL(0,3)
+
+# Print the values of the linked list
+print([node.value for node in singlyLinkedList])
+
+# Traverse the linked list
+# singlyLinkedList.traverseSLL()
+
+# Search for a node in the linked list
+print(singlyLinkedList.searchSLL(33))
+
+# Delete a node from the linked list
+singlyLinkedList.deleteNode(2)
+print([node.value for node in singlyLinkedList])
